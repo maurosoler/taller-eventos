f77a0fe5a8873fd3abac90e5556b797946383dd4
maurosoler <soler.tech.12@gmail.com>
2023-08-11 21:06:31 -0300

    Creado el Alert del boton y del div en JS

---
+++ eventos.js
@@ -0,0 +1,13 @@
+let btn = document.getElementById('miboton');
+
+btn.addEventListener('click', function() {
+    alert('Hola!');
+    });
+
+
+
+let div = document.getElementById('miDiv');
+
+div.addEventListener('click', function() {
+  alert('Hola! Soy el div');
+});
